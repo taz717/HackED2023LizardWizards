dafcb9da8ad4a68a8528aa0f71064d939db7195c
taz717 <moatazkhallaf@outlook.com>
2023-01-07 21:55:31 -0700

    -add move recognition in list to send to arduino -add todo (send vals to ardy) -add if maine run command for coding standards

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from lib import kingGizzard as kg
 import chess as ch
 
+numToLetterDict = {1: "A", 2: "B", 3: "C", 4: "D", 5: "E", 6: "F", 7: "G", 8: "H"}
+
 
 class Main:
     def __init__(self, board=ch.Board):
@@ -22,6 +24,35 @@ class Main:
         except:
             self.playOpponentMove()
 
+    def makeMatrix(self, board):  # type(board) == chess.Board()
+        ## https://stackoverflow.com/questions/55876336/is-there-a-way-to-convert-a-python-chess-board-into-a-list-of-integers
+
+        ## returns 2d array with board
+
+        pgn = board.epd()
+        foo = []  # Final board
+        pieces = pgn.split(" ", 1)[0]
+        rows = pieces.split("/")
+        for row in rows:
+            foo2 = []  # This is the row I make
+            for thing in row:
+                if thing.isdigit():
+                    for i in range(0, int(thing)):
+                        foo2.append(".")
+                else:
+                    foo2.append(thing)
+            foo.append(foo2)
+        return foo
+
+    def compareBoards(self, matrix1, matrix2):
+        vals = []
+        for i in range(len(matrix1)):
+            for j in range(len(matrix1)):
+                if matrix1[i][j] != matrix2[i][j]:
+                    vals.append(numToLetterDict[j + 1] + str(i + 1))
+
+        return vals
+
     ## play king gizzard move
     def playEngineMove(self, maxDepth, color):
         engine = kg.kingGizzard(self.board, maxDepth, color)
@@ -41,11 +72,16 @@ class Main:
         if color == "b":
             while self.board.is_checkmate() == False:
                 print("King Gizzard is thinking...")
-                temp = self.board
+
+                previousBoard = self.makeMatrix(self.board)
                 self.playEngineMove(maxDepth, ch.WHITE)
-                self.translateBoards(temp)
                 print(self.board)
+                vals = self.compareBoards(previousBoard, self.makeMatrix(self.board))
+                ## TODO SEND VALS TO ARDUINO TO MOVE PIECES
+                print(vals)
+
                 self.playOpponentMove()
+
             print(self.board)
             print(self.board.outcome())
         elif color == "w":
@@ -53,8 +89,14 @@ class Main:
                 print(self.board)
                 self.playOpponentMove()
                 print(self.board)
+
                 print("King Gizzard is thinking...")
+                previousBoard = self.makeMatrix(self.board)
                 self.playEngineMove(maxDepth, ch.BLACK)
+                vals = self.compareBoards(previousBoard, self.makeMatrix(self.board))
+                ## TODO SEND VALS TO ARDUINO TO MOVE PIECES
+                print(vals)
+
             print(self.board)
             print(self.board.outcome())
 
@@ -64,6 +106,7 @@ class Main:
             self.startGame()
 
 
-newBoard = ch.Board()
-game = Main(newBoard)
-game.startGame()
+if __name__ == "__main__":
+    newBoard = ch.Board()
+    game = Main(newBoard)
+    game.startGame()
