8149547003c890bf9fd7182609a52b4ad6189ff5
Ali1215 <amuneer@ualberta.ca>
2023-01-08 06:23:46 +0530

    new arduino class, gonna use it for sending and recieving stuff

--- lib/serialControl.py
+++ lib/serialControl.py
@@ -1,25 +1,44 @@
 import time
 import serial
 # TODO move to main.py
-arduinoData = serial.Serial('com6', 115200)
-time.sleep(1)
-i = 0
+
 
 # Infinite loop to watch for serial comms from Arduino
 # TODO move loop to main.py
-while True:
-    # If the arduino has bytes waiting to be read, read them
-    if arduinoData.inWaiting() != 0:
+class ArduinoIO:
+    def __init__(self) -> None:
+        self.arduinoData = serial.Serial('com6', 115200)
+
+    def readData(self):
+        while arduinoData.inWaiting() == 0:
+            pass #block until there is something to read
+
         dataPacket = arduinoData.readline()
         dataPacket = str(dataPacket, 'utf-8')
         dataPacket = dataPacket.strip('\r\n')
         # Print arduino output into terminal
-        print(dataPacket)
-        time.sleep(0.1)
-    else:
-        # Accept user input
-        # TODO change this to output of AI
-        cmd = input('Enter command: ')
-        # Write command to serial
+        return (dataPacket)
+    
+    def writeData(str):
         arduinoData.write(str.encode(cmd+'\r\n'))
-        time.sleep(0.5)
+
+if __name__ == "__main__":
+    arduinoData = serial.Serial('com6', 115200)
+    time.sleep(1)
+    i = 0
+    while True:
+        # If the arduino has bytes waiting to be read, read them
+        if arduinoData.inWaiting() != 0:
+            dataPacket = arduinoData.readline()
+            dataPacket = str(dataPacket, 'utf-8')
+            dataPacket = dataPacket.strip('\r\n')
+            # Print arduino output into terminal
+            print(dataPacket)
+            time.sleep(0.1)
+        else:
+            # Accept user input
+            # TODO change this to output of AI
+            cmd = input('Enter command: ')
+            # Write command to serial
+            arduinoData.write(str.encode(cmd+'\r\n'))
+            time.sleep(0.5)
