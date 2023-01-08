41d1d6d777c5123b405292ea773d897af22e72d8
taz717 <moatazkhallaf@outlook.com>
2023-01-07 17:25:24 -0700

    -add game in terminal -finish engine

--- AI/kingGizzard.py
+++ AI/kingGizzard.py
@@ -1,4 +1,5 @@
 import chess as ch
+import random as rd
 
 
 class kingGizzard:
@@ -6,6 +7,59 @@ class kingGizzard:
         self.board = board
         self.maxDepth = maxDepth
         self.color = color
+
+    def getBestMove(self):
+        return self.engine(None, 1)
+
+    def evaluate(self):
+        compt = 0
+        for i in range(64):
+            compt += self.squareResPoints(ch.SQUARES[i])
+        compt += self.mateOppurtunity() + self.opening() + 0.001 * rd.random()
+
+    def opening(self):
+        if self.board.fullmove_number < 10:
+            if self.board.turn == self.color:
+                return 1 / 30 * self.board.legal_moves.count()
+            else:
+                return -1 / 30 * self.board.legal_moves.count()
+        else:
+            return 0
+
+    def mateOppurtunity(self):
+        if self.board.legal_moves.count() == 0:
+            ## engine getting checked
+            if self.board.turn == self.color:
+                -999
+
+            else:
+                ## opponent getting checked
+                return 999
+
+        ## there is a legal move
+        else:
+            return 0
+
+    ## takes a square as input and returns
+    ## Han's Berlinder's system value of its resident
+    ## https://www.chessprogramming.org/Hans_Berliner
+    def squareResPoints(self, square):
+        pieceValue = 0
+        if self.board.piece_type_at(square) == ch.PAWN:
+            pieceValue = 1
+        elif self.board.piece_type_at(square) == ch.KNIGHT:
+            pieceValue = 3.2
+        elif self.board.piece_type_at(square) == ch.BISHOP:
+            pieceValue = 3.33
+        elif self.board.piece_type_at(square) == ch.ROOK:
+            pieceValue = 5.1
+        elif self.board.piece_type_at(square) == ch.QUEEN:
+            pieceValue = 8.8
+
+        if self.board.color_at(square) != self.color:
+            return -pieceValue
+        else:
+            return pieceValue
 
     def engine(self, candidate, depth):
         if depth == self.maxDepth or self.board.legal_moves.count() == 0:
@@ -45,4 +99,21 @@ class kingGizzard:
                     newCandidate = value
 
                 ## alpha-beta pruning
-                ## if previous candidate is better than new candidate
+                ## if previous move was made by engine
+                if candidate != None and value < candidate and depth % 2 == 0:
+                    self.board.pop()
+                    break
+
+                ## if previous move was made by opponent
+                elif candidate != None and value > candidate and depth % 2 != 0:
+                    self.board.pop()
+                    break
+
+                ## undo last move
+                self.board.pop()
+
+        if depth > 1:
+            ## return value of the node in the tree
+            return newCandidate
+        else:
+            return move
