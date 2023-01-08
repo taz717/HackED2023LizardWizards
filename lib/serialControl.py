d060dd5b02cf01d9f15a05d1aeb408569356f994
RJ Forgie <rforgie@ualberta.ca>
2023-01-07 22:14:37 -0700

    Map path, print gcode

--- lib/serialControl.py
+++ lib/serialControl.py
@@ -3,11 +3,10 @@ import serial
 # TODO move to main.py
 arduinoData = serial.Serial('com6', 115200)
 time.sleep(1)
-i = 0
 
 # Infinite loop to watch for serial comms from Arduino
 # TODO move loop to main.py
-while True:
+def serialControl():
     # If the arduino has bytes waiting to be read, read them
     if arduinoData.inWaiting() != 0:
         dataPacket = arduinoData.readline()
--- lib/translate.py
+++ lib/translate.py
@@ -1,6 +1,7 @@
 import numpy as np
-COLS = "ABCDEFGH"
 
+COLS = "ABCDEFGH"
+SQUARESIZE = 1.5 # inches
 
 def moveToPath(move, displace=None):
     # Find initial position
@@ -20,6 +21,32 @@ def moveToPath(move, displace=None):
     diffSquares = finalSquares - initialSquares
     print(diffSquares)
 
+    # Move the magnet
+    travel(initialSquares)
 
+    paths = []
+    # Move out of the square
+    paths.append(np.ones(2)*SQUARESIZE/2)
+    paths.append(np.array([diffSquares[0]*SQUARESIZE,0]))
+    paths.append(np.array([0,diffSquares[1]*SQUARESIZE]))
+    paths.append(-paths[0])
+    slide(paths)
+
+# Move the magnet in a straight line to 'absPosition' while magnet disabled (format: [x_pos,y_pos])
+def travel(absPosition):
+    sendCommand("M03") # turn off magnet
+    travelCommand = "G91 X"+str(absPosition[0])+" Y"+str(absPosition[1]) # G91 means absolute coords
+    sendCommand(travelCommand)
+
+# Move the magnet along a set of 'paths' while dragging a piece (format: [[x1_dist,y1_dist],[x2_dist,y2_dist],...]])
+def slide(paths):
+    sendCommand("M04")
+    for path in paths:
+        moveCommand = "G92 X"+str(path[0])+" Y"+str(path[1]) # G92 means relative coords
+        sendCommand(moveCommand)
+
+def sendCommand(command):
+    # TODO send through serialControl.py
+    print(">",command)
 
 moveToPath(['E1','B5'])
